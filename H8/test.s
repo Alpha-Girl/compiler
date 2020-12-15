05f36f89e7e394180eab8722177af9e81700a005
Alpha-Girl <alphaboy@mail.ustc.edu.cn>
2020-12-15 19:31:32 +0800

    H8

---
+++ H8/test.s
@@ -0,0 +1,42 @@
+	.file	"test.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"%ld, %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movq	$4, -32(%rbp)
+	movq	$8, -24(%rbp)
+	movq	-16(%rbp), %rax
+	movq	%rax, %rdx
+	movl	$0, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	movq	-8(%rbp), %rcx
+	xorq	%fs:40, %rcx
+	je	.L3
+	call	__stack_chk_fail@PLT
+.L3:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 7.5.0-3ubuntu1~18.04) 7.5.0"
+	.section	.note.GNU-stack,"",@progbits
